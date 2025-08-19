79e125e318a58167a4ab37c0f5e5d880a3e52f57
Devendar Reddy <104640575+DEREAME@users.noreply.github.com>
2025-08-19 22:39:41 +0530

    Adding for loop

---
+++ day-1/forloop.py
@@ -0,0 +1,14 @@
+for i in range (1,20):
+    print (i)
+
+
+fruits=["apple","banana","mango","grapes"]
+
+for i in fruits:
+    print (i)
+
+name ="DEVENDAR"
+for char in name:
+    print(char)
+
+    
